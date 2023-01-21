3cb5abeaf87ee81b78a85c46a007b4119d6275be
TuuKeZu <moilanentuukka5@gmail.com>
2023-01-21 16:55:51 +0200

    Terminal shouldn't instantly exist after running

--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,9 @@ fn main() {
     println!("Hello.rs v{VERSION}");
     println!("-------------------");
     println!("> Hello, world!");
+
+
+    // terminal shouldn't terminate on close
+    println!("Press any key to continue...");
+    std::io::stdin().read_line(&mut String::new()).unwrap();
 }
